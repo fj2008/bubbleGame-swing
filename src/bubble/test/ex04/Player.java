246417bfafa3eb72a7970a08b1689390bb14af6a
unknown <fj2008@naver.com>
2021-12-05 11:08:43 +0900

    케릭터 좌우 이동완료/각각 움직임마다 스레드사용해서 부드럽게 이동하도록 만들었습니다.

---
+++ src/bubble/test/ex04/Player.java
@@ -0,0 +1,101 @@
+package bubble.test.ex04;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+import lombok.Getter;
+import lombok.Setter;
+
+//class Player -> new 가능한 애들 게임에 존재할 수 있다. (추상메서드를 가질 수 없다)
+@Getter
+@Setter
+public class Player extends JLabel implements Moveable {
+
+	//위치상태
+	private int x;
+	private int y;
+
+	//움직임 상태
+	private boolean left;
+	private boolean right;
+	private boolean up;
+	private boolean down;
+	
+	
+	private ImageIcon playerR, playerL;
+
+	public Player() {
+		initObject();
+		initSetting();
+	}
+
+	private void initObject() {
+		playerR = new ImageIcon("image/playerR.png");
+		playerL = new ImageIcon("image/playerL.png");
+
+	}
+
+	private void initSetting() {
+
+		x = 55;
+		y = 535;
+
+		left = false;
+		right = false;
+		up = false;
+		down = false;
+		
+		
+		setIcon(playerR);
+		setSize(50, 50);
+		setLocation(x, y);
+	}
+
+	@Override
+	public void left() {
+		
+		left =true;
+		new Thread(()-> {
+			while(left) {
+				setIcon(playerL);
+				x= x-1;
+				setLocation(x,y);
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		
+	}
+
+	@Override
+	public void right() {
+		right = true;
+		new Thread(()-> {
+			while(right) {
+				setIcon(playerR);
+				x= x+1;
+				setLocation(x,y);
+				try {
+					Thread.sleep(10);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	
+	}
+
+	@Override
+	public void up() {
+
+	}
+
+	@Override
+	public void down() {
+
+	}
+
+}
